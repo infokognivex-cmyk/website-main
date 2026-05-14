de1e9a1da3eef03545cba8ea0ee9fad3252872f0
vinay bhaskar <bhaskarvinaychitturi@gmail.com>
2026-05-14 23:54:21 +0530

    sixth commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ["https://website-frountend.onrender.com", "http://localhost:3000"],
+  origin: ["https://kognivex.in", "https://website-frountend.onrender.com", "http://localhost:3000"],
   credentials: true
 }));
 app.use(express.json());
