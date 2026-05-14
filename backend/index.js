6afdd6edf81c3abe67631750762a25a77576d4bd
vinay bhaskar <bhaskarvinaychitturi@gmail.com>
2026-05-14 19:12:20 +0530

    fourth

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ["https://website-frountend.onrender.com", "http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
